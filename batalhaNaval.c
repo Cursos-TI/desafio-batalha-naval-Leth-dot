ce72b91ab37b6aa509d418bc371e2bc7f1e28cf4
Leth-dot <202507318616@alunos.estacio.br>
2025-11-11 20:19:36 +0000

    primeiro código do desafio

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+printf("-- Batalha Naval --\n");
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+//declarando a matriz que representa a água
+int agua[10][10];
+int linha, coluna;
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+//inicializando o tabuleiro através de loops aninhados
+for (linha = 0; linha < 10; linha++) {
+    for (coluna = 0; coluna < 10; coluna++) {
+        agua[linha][coluna] = 0;
+    }
+}
 
-    return 0;
+//declarando, inicializando e posicionando os navios, substituindo os valores da matriz original
+agua[1][2] = 3;
+agua[1][3] = 3;
+agua[1][4] = 3; //horizontal
+
+agua[5][5] = 3;
+agua[6][5] = 3;
+agua[7][5] = 3; //vertical
+
+//imprimindo tudo na tela
+for (linha = 0; linha < 10; linha++){
+    for (coluna = 0; coluna < 10; coluna++)
+    {
+        printf("%d", agua[linha][coluna]);
+    }
+
+    printf("\n");
+}
+
+
+return 0;
+
 }
